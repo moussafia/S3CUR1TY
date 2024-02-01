cfd7d8fe925303f1fba26794537e4229c7224047
moussafia <moussafia.mohammed1999@gmail.com>
2024-02-01 22:19:10 +0100

    first commit

---
+++ thirdParty/src/main/java/ma/youcode/thirdparty/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ma.youcode.thirdparty;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThirdPartyApplication.class);
+	}
+
+}
---
+++ thirdParty/src/test/java/ma/youcode/thirdparty/ThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
