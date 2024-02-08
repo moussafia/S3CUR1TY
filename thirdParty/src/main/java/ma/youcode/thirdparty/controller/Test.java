d6a2db296bc908e9333aea0d5573e9d885e338f3
moussafia <moussafia.mohammed1999@gmail.com>
2024-02-08 13:23:33 +0100

    terminer configuration keycloak

--- thirdParty/src/main/java/ma/youcode/thirdparty/controller/Test.java
+++ thirdParty/src/main/java/ma/youcode/thirdparty/controller/Test.java
@@ -1,9 +1,11 @@
 package ma.youcode.thirdparty.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -12,5 +14,10 @@ public class Test {
     public String testApi(){
 
         return "test";
+    }
+    @GetMapping("/auth")
+    @ResponseBody
+    public Authentication authentication(Authentication authentication){
+        return authentication;
     }
 }
